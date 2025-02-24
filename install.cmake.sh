d644ef6d399e6f73de426f0f62d6eb8688157fd9
Shaoheng Guan <191359602@qq.com>
2025-02-24 15:17:35 +0000

    test

--- install.cmake.sh
+++ install.cmake.sh
@@ -11,5 +11,3 @@ cmake_bin=cmake
 ctest_bin=ctest
 
 rm -rf build_cmake && mkdir build_cmake && cd build_cmake && ${cmake_bin}  -DCMAKE_BUILD_TYPE=Release -S.. -B. && ${cmake_bin}  --build . --target all --config Release -j 4 # && ${ctest_bin} -j4 --verbose
-
-Echo "Done"
